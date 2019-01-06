cf79b972f85082a7dcf62e6fc0d279b25278c2e2
Vincent Heeren <vincent.heeren@student.ucll.be>
2019-01-06 17:20:38 +0100

    comment consul zodat het lokaal werkt

--- src/main/java/DenTravak/Application.java
+++ src/main/java/DenTravak/Application.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,7 +21,7 @@ import static DenTravak.domain.Sandwich.SandwichBuilder.aSandwich;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class Application {
 
     //private SandwichRepository srepository;
@@ -41,10 +41,10 @@ public class Application {
         };
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
 
     @Configuration
     public class WebConfig implements WebMvcConfigurer {
--- src/main/java/DenTravak/controller/SandwichController.java
+++ src/main/java/DenTravak/controller/SandwichController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+//import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import javax.naming.ServiceUnavailableException;
@@ -18,14 +18,14 @@ import java.util.Optional;
 public class SandwichController {
 
     //private List<Sandwich> sandwiches = new ArrayList<Sandwich>();
-    @Autowired
-    private DiscoveryClient discoveryClient;
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
 
     @Autowired
     private SandwichRepository repository;
 
-    @Autowired
-    private RestTemplate restTemplate;
+//    @Autowired
+//    private RestTemplate restTemplate;
 
     //private OrderRepository orepository;
 
@@ -38,15 +38,15 @@ public class SandwichController {
     @RequestMapping("/sandwiches")
     public Iterable<Sandwich> sandwich() {
         // lijst van sandwiches
-        //return repository.findAll();
-        try {
-            Preferences preferences = getPreferences("ronald.dehuysser@ucll.be");
-            //TODO: sort allSandwiches by float in preferences
-            Iterable<Sandwich> allSandwiches = repository.findAll();
-            return allSandwiches;
-        } catch (ServiceUnavailableException e) {
-            return repository.findAll();
-        }
+        return repository.findAll();
+//        try {
+//            Preferences preferences = getPreferences("ronald.dehuysser@ucll.be");
+//            //TODO: sort allSandwiches by float in preferences
+//            Iterable<Sandwich> allSandwiches = repository.findAll();
+//            return allSandwiches;
+//        } catch (ServiceUnavailableException e) {
+//            return repository.findAll();
+//        }
     }
 
     @RequestMapping(value="/sandwiches", method= RequestMethod.POST)
@@ -84,20 +84,20 @@ public class SandwichController {
     }
 
     // why comment: for testing
-    @GetMapping("/getpreferences/{emailAddress}")
-    public Preferences getPreferences(@PathVariable String emailAddress) throws RestClientException, ServiceUnavailableException {
-        URI service = recommendationServiceUrl()
-                .map(s -> s.resolve("/recommend/" + emailAddress))
-                .orElseThrow(ServiceUnavailableException::new);
-        return restTemplate
-                .getForEntity(service, Preferences.class)
-                .getBody();
-    }
+//    @GetMapping("/getpreferences/{emailAddress}")
+//    public Preferences getPreferences(@PathVariable String emailAddress) throws RestClientException, ServiceUnavailableException {
+//        URI service = recommendationServiceUrl()
+//                .map(s -> s.resolve("/recommend/" + emailAddress))
+//                .orElseThrow(ServiceUnavailableException::new);
+//        return restTemplate
+//                .getForEntity(service, Preferences.class)
+//                .getBody();
+//    }
 
-    public Optional<URI> recommendationServiceUrl() {
-        return discoveryClient.getInstances("recommendation")
-                .stream()
-                .map(si -> si.getUri())
-                .findFirst();
-    }
+//    public Optional<URI> recommendationServiceUrl() {
+//        return discoveryClient.getInstances("recommendation")
+//                .stream()
+//                .map(si -> si.getUri())
+//                .findFirst();
+//    }
 }
